3c10a3e6cc51302ce6905b805998983bbf7d124a
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2026-02-23 17:12:37 -0300

    Ordenacao de strings na linguagem c com o selecao

--- PucMinas/Microfundamentos/algoritmoSelecao.c
+++ PucMinas/Microfundamentos/algoritmoSelecao.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void swap(int* vet, int i, int j){
 	int temp = vet[i];
@@ -7,14 +8,14 @@ void swap(int* vet, int i, int j){
 	vet[j] = temp;
 }
 
-void imprimeVetor(int* vet, int tamVet){
+/*void imprimeVetor(char* vet[], int tamVet){
     for(int i = 0; i < tamVet; i++){   
-	    printf("%d\n", vet[i]);
+	    printf("%s\n", vet[i]);
     }
-}
+}*/
 
 //Versao Original
-void ordenacaoSelecao(int* vet, int tam){
+/*void ordenacaoSelecao(int* vet, int tam){
 	for(int i = 0; i < (tam - 1); i++){
 		int menor = i;
 		for(int j = (i + 1); j < tam; j++){
@@ -26,10 +27,10 @@ void ordenacaoSelecao(int* vet, int tam){
         printf("Passagem: %d\n", i);
         imprimeVetor(vet, tam);
 	}
-}
+}*/
 
 //Versao Decrescente
-void selecaoDecrescente(int* vet, int tam){
+/*void selecaoDecrescente(int* vet, int tam){
 	for(int i = 0; i < (tam - 1); i++){
 		int maior = i;
 		for(int j = (i + 1); j < tam; j++){
@@ -41,8 +42,7 @@ void selecaoDecrescente(int* vet, int tam){
         printf("Passagem: %d\n", i);
         imprimeVetor(vet, tam);
 	}
-}
-
+}*/
 
 void preencheVetor(int* vet, int tamVet){
     printf("Preencher Vetor: \n");
@@ -51,17 +51,80 @@ void preencheVetor(int* vet, int tamVet){
 	}
 }
 
+bool compararStrings(char* p1, char* p2){
+	bool resultado = true, terminou = true;
+	int i = 0;
+	while(p1[i] != '\0' && p2[i] != '\0' && terminou){
+		if(p1[i] < p2[i]){
+			resultado = false; //a string p1 eh menor
+			terminou = false;
+		}else if(p1[i] > p2[i]){
+			terminou = false; //a string p2 eh maior
+		}else{
+			i++;
+		}
+	}
+
+	//Se uma palavra termina primeiro, ela vem antes. (Ana, Anabele)
+	if (terminou == false) {
+        if (p1[i] == '\0' && p2[i] != '\0') { //A string p1 terminou e a p2 ainda tem letras o resultado vai ser que a string p1 vem antes.
+            resultado = false;
+        }
+        else if (p1[i] != '\0' && p2[i] == '\0') { //A string p1 nao terminou e a p2 terminou o resultado vai ser que a string p1 vem depois.
+            resultado = true;
+        }
+    }
+
+	return resultado;
+}
+
+void ordenarAlfabetico(char* vetor[], int tam) {
+
+    for (int i = 0; i < tam - 1; i++) {
+
+        int menor = i;
+
+        for (int j = i + 1; j < tam; j++) {
+
+            if (compararStrings(vetor[j], vetor[menor]) == false) {
+                menor = j;
+            }
+        }
+
+        if (menor != i) {
+            char* temp = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = temp;
+        }
+    }
+}
 
 int main(){
-	
 	int tamVet = 0;
     printf("Tamanho vetor: \n");
-	scanf("%d", &tamVet);	
-	int vetor[tamVet];
-	preencheVetor(vetor, tamVet);
-	selecaoDecrescente(vetor, tamVet);
-	printf("Vetor Ordenado: \n");
-	imprimeVetor(vetor, tamVet);
+    scanf("%d", &tamVet);
+    getchar(); 
 
-	return 0;
+    
+    char vetorChar[tamVet][100]; 
+
+    for (int i = 0; i < tamVet; i++) {
+        scanf(" %[^\n]", vetorChar[i]);
+    }
+
+    
+    char* ponteiros[tamVet];
+    for (int i = 0; i < tamVet; i++) {
+        ponteiros[i] = vetorChar[i];
+    }
+
+    ordenarAlfabetico(ponteiros, tamVet);
+
+    printf("Vetor Ordenado:\n");
+    for (int j = 0; j < tamVet; j++) {
+        printf("%s\n", ponteiros[j]);
+    }
+
+    return 0;
+
 }
