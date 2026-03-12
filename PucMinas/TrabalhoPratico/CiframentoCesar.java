0d35971e7190cb836c6efecef5f17f16d1815446
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2026-03-12 09:10:59 -0300

    TP01 - Q01 - CiframentoCesar

---
+++ PucMinas/TrabalhoPratico/CiframentoCesar.java
@@ -0,0 +1,33 @@
+package PucMinas.TrabalhoPratico;
+import java.util.Scanner;
+
+public class CiframentoCesar {
+    
+    public static String cifraString(String palavra, int chaveCifra){
+        String cifrada = "";
+        for(int i = 0; i < palavra.length(); i++){
+            char modificado = palavra.charAt(i);
+            if(modificado > 'A' && modificado < 'Z' || modificado > 'a' && modificado < 'z'){
+                modificado = (char) (modificado + chaveCifra);
+                cifrada += modificado;
+            }else{
+                cifrada += modificado;
+            }
+        }
+        return cifrada;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palavra = sc.nextLine();
+        
+        while(palavra.length() != 3 || palavra.charAt(0) != 'F' && palavra.charAt(1) != 'I' && palavra.charAt(2) != 'M'){
+            
+            String cifrada = cifraString(palavra, 3);
+            System.out.println(cifrada);
+            palavra = sc.nextLine();
+
+        }
+        sc.close();
+    }
+}
