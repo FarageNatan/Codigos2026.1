4bcce6d0bd119defd63a03a0d535a43e3c00693d
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2026-03-23 18:13:52 -0300

    Soma de digitos de um numero - Correta

---
+++ PucMinas/TrabalhoPratico/SomaDigitosRec.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tamanhoString(char* string){
+    int tam = 0;
+    while(string[tam] != '\0'){
+        tam++;
+    }
+    return tam;
+}
+
+int somaDigitos(int numero){
+    int resp = 0;
+    if(numero > 0){
+        resp = (numero % 10 + somaDigitos(numero / 10));
+    }
+    return resp;
+}
+
+int transformaNumero(char* string){
+    int num = 0;
+    
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] >= '0' && string[i] <= '9') {
+            num = num * 10 + (string[i] - '0');
+        }
+    }
+
+    return num;
+}
+
+int main(){
+    char* numero = malloc(1000 * sizeof(char));
+    fgets(numero, 1000, stdin);
+    int tam = tamanhoString(numero);
+    if (tam > 0 && numero[tam - 1] == '\n') {
+        numero[tam - 1] = '\0';
+        tam--;
+    }
+    while(!(numero[0] == 'F' && numero[1] == 'I' && numero[2] == 'M' && tam == 3)){
+        int num = transformaNumero(numero);
+        int soma = somaDigitos(num);
+        printf("%d\n", soma);
+
+        fgets(numero, 1000, stdin);
+        tam = tamanhoString(numero);
+        if (tam > 0 && numero[tam - 1] == '\n') {
+            numero[tam - 1] = '\0';
+            tam--;
+        }
+    }
+
+    free(numero);
+    return 0;   
+}
