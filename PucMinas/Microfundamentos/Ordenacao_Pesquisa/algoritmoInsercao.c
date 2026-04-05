92f8dece6766ad011b19dbde80504bbac9343389
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2026-04-05 17:49:31 -0300

    Ordenacao por insercao - Pesquisa Binaria

--- PucMinas/Microfundamentos/Ordenacao_Pesquisa/algoritmoInsercao.c
+++ PucMinas/Microfundamentos/Ordenacao_Pesquisa/algoritmoInsercao.c
@@ -13,6 +13,36 @@ void insercao(int array[], int n){
 	}//for
 }
 
+int pesquisa(int array[], int tmp, int fim) {
+    int esq = 0;
+    int dir = fim;
+    
+    while (esq <= dir) {
+        int meio = esq + (dir - esq) / 2;
+        
+        if (array[meio] < tmp) {
+            esq = meio + 1;
+        } else {
+            dir = meio - 1;
+        }
+    }
+    return esq;
+}
+
+
+void insercaoBinaria(int array[], int n){
+	for(int i = 1; i < n; ++i){
+		int tmp = array[i];
+		int pos = pesquisa(array, tmp, i - 1);
+		int j = i - 1;	
+		while(j >= pos){
+			array[j + 1] = array[j];
+			j--;
+		}//while
+		array[pos] = tmp;
+	}//for
+}
+
 int main(){
 	int tam;
 	scanf("%d", &tam);
@@ -29,7 +59,7 @@ int main(){
 	}
     printf("\n");
 	
-	insercao(array, tam);
+	insercaoBinaria(array, tam);
 
 	//Ordenado
 	for(int i = 0; i < tam; i++){
