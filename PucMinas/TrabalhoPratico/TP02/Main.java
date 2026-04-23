58eaff075de6d8f1c0d441c0e924f1a5e13d02e0
Natan Farage Cohen <natan.cohen@sga.pucminas.br>
2026-04-23 18:58:39 -0300

    Atualizações do tp02

--- PucMinas/TrabalhoPratico/TP02/Main.java
+++ PucMinas/TrabalhoPratico/TP02/Main.java
@@ -1,14 +1,44 @@
 package PucMinas.TrabalhoPratico.TP02;
 
 class Data{
-    int ano;
-    int mes;
-    int dia;
+    private int ano;
+    private int mes;
+    private int dia;
+
+    public Data(int ano, int mes, int dia) {
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
 }
 
 class Hora{
-    int hora;
-    int minuto;
+    private int hora;
+    private int minuto;
 
     public Hora(int h, int min){
         if(h < 0 || h > 23 || min < 0 || min > 59){
@@ -24,18 +54,94 @@ class Hora{
         minuto = 0;
     }
 
+    public int getHora() {
+        return hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
 }
 
 
 class Restaurante{
-    int id;
-    String nome;
-    String cidade;
-    int capacidade;
-    double avaliacao;
-    String[] tiposCozinha;
-    int faixaPreco;
+    private int id;
+    private String nome;
+    private String cidade;
+    private int capacidade;
+    private double avaliacao;
+    private String[] tiposCozinha;
+    private int faixaPreco;
+    private Hora horarioAbertura;
+    private Hora horarioFechamento;
+    private Data dataAbertura;
+    private Boolean aberto;
 
+    public Restaurante(int id, String nome, String cidade, int capacidade, double avaliacao, String[] tiposCozinha,int faixaPreco, Hora horarioAbertura, Hora horarioFechamento, Data dataAbertura, Boolean aberto) {
+        this.id = id;
+        this.nome = nome;
+        this.cidade = cidade;
+        this.capacidade = capacidade;
+        this.avaliacao = avaliacao;
+        this.tiposCozinha = tiposCozinha;
+        this.faixaPreco = faixaPreco;
+        this.horarioAbertura = horarioAbertura;
+        this.horarioFechamento = horarioFechamento;
+        this.dataAbertura = dataAbertura;
+        this.aberto = aberto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public String[] getTiposCozinha() {
+        return tiposCozinha;
+    }
+
+    public int getFaixaPreco() {
+        return faixaPreco;
+    }
+
+    public Hora getHorarioAbertura() {
+        return horarioAbertura;
+    }
+
+    public Hora getHorarioFechamento() {
+        return horarioFechamento;
+    }
+
+    public Data getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public Boolean getAberto() {
+        return aberto;
+    }
 }
 
 
